293efaa785a8752dbbf76edf85c6fa19a28b1295
Sebastian Meyer <git@ripa-alta.de>
2025-12-02 06:26:50 +0100

    Cleaned up Day 01

--- src/test/java/de/aoc/y2025/Day01Test.java
+++ src/test/java/de/aoc/y2025/Day01Test.java
@@ -5,38 +5,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day01Test extends AoCTest {
+
+    private final String input = """
+          L68
+          L30
+          R48
+          L5
+          R60
+          L55
+          L1
+          L99
+          R14
+          L82
+          """;
+
     @Test
     public void testPartOne() {
-        String input = """
-              L68
-              L30
-              R48
-              L5
-              R60
-              L55
-              L1
-              L99
-              R14
-              L82
-              """;
         assertEquals(3, Day01.partOne(input));
         assertEquals(1180, Day01.partOne(readInput()));
     }
 
     @Test
     public void testPartTwo() {
-        String input = """
-              L68
-              L30
-              R48
-              L5
-              R60
-              L55
-              L1
-              L99
-              R14
-              L82
-              """;
         assertEquals(6, Day01.partTwo(input));
         assertEquals(6892, Day01.partTwo(readInput()));
     }
