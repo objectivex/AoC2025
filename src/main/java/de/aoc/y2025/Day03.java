6eca62a4c8b3081413b05c2ae522a18f1ebed069
Sebastian Meyer <git@ripa-alta.de>
2025-12-03 07:33:58 +0100

    Cleaned up Part 03

--- src/main/java/de/aoc/y2025/Day03.java
+++ src/main/java/de/aoc/y2025/Day03.java
@@ -14,37 +14,6 @@ public class Day03 {
         return sum;
     }
 
-    private static long getJolt(String line, int size) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < line.length(); i++) {
-            numbers.add(Integer.parseInt(line.substring(i, i + 1)));
-        }
-
-        int startIndex = 0;
-        long max = 0;
-        for (int i = size - 1; i >= 0; i--) {
-            var head = numbers.subList(startIndex, numbers.size()  - i);
-            int localMax = head.stream().mapToInt(n -> n).max().orElseThrow();
-            startIndex = startIndex+head.indexOf(localMax) + 1;
-            max = 10 * max + localMax;
-        }
-        return max;
-    }
-
-    private static long getJolt(List<Integer> integers, long current, int remaining) {
-        if (remaining == 0) {
-            return current;
-        }
-        long max = 0;
-        for (int i1 = 0; i1 < integers.size(); i1++) {
-            long returnValue = current * 10 + integers.get(i1);
-
-            max = Math.max(max, getJolt(integers.subList(i1 + 1, integers.size()), returnValue, remaining - 1));
-        }
-
-        return max;
-    }
-
     public static long partTwo(String input) {
         var lines = input.lines().toList();
 
@@ -55,4 +24,20 @@ public class Day03 {
         return sum;
     }
 
+    private static long getJolt(String line, int size) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < line.length(); i++) {
+            numbers.add(Integer.parseInt(line.substring(i, i + 1)));
+        }
+
+        int startIndex = 0;
+        long max = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            var head = numbers.subList(startIndex, numbers.size() - i);
+            int localMax = head.stream().mapToInt(n -> n).max().orElseThrow();
+            startIndex = startIndex + head.indexOf(localMax) + 1;
+            max = 10 * max + localMax;
+        }
+        return max;
+    }
 }
