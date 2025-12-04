a49a02feb27f2e12c64823c9340f6bb13cbac790
Sebastian Meyer <git@ripa-alta.de>
2025-12-04 06:33:02 +0100

    Cleanup for Day 04

--- src/main/java/de/aoc/y2025/Day04.java
+++ src/main/java/de/aoc/y2025/Day04.java
@@ -3,20 +3,56 @@ package de.aoc.y2025;
 public class Day04 {
 
     public static long partOne(String input) {
-        var lines = input.split("\n");
+        var grid = createGrid(input);
+        identifyPaperrollsToMove(grid);
+        return countMovablePaperrolls(grid);
+    }
 
-        var grid = new char[lines.length][lines[0].length()];
+    public static long partTwo(String input) {
+        var grid = createGrid(input);
 
-        for (int y = 0; y < lines.length; y++) {
-            var line = lines[y];
-            for (int x = 0; x < line.length(); x++) {
-                grid[y][x] = line.charAt(x);
+        var cnt = 0;
+        while (true) {
+            identifyPaperrollsToMove(grid);
+
+            var currentCnt = countAndRemovePaperrolls(grid);
+
+            if (currentCnt == 0) {
+                break;
+            }
+
+            cnt += currentCnt;
+        }
+        return cnt;
+    }
+
+    private static int countMovablePaperrolls(char[][] grid) {
+        int cnt = 0;
+        for (char[] line : grid) {
+            for (char c : line) {
+                if (c == 'x') {
+                    cnt++;
+                }
             }
         }
+        return cnt;
+    }
 
+    private static int countAndRemovePaperrolls(char[][] grid) {
+        int cnt = 0;
+        for (int y = 0; y < grid.length; y++) {
+            var line = grid[y];
+            for (int x = 0; x < line.length; x++) {
+                if (grid[y][x] == 'x') {
+                    grid[y][x] = '.';
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
 
-//        var checkedGrid = new char[lines.length][lines[0].length()];
-
+    private static void identifyPaperrollsToMove(char[][] grid) {
         for (int y = 0; y < grid.length; y++) {
             var line = grid[y];
             for (int x = 0; x < line.length; x++) {
@@ -32,19 +68,20 @@ public class Day04 {
                 }
             }
         }
+    }
 
+    private static char[][] createGrid(String input) {
+        var lines = input.split("\n");
 
-        int cnt = 0;
-        for (int y = 0; y < grid.length; y++) {
-            var line = grid[y];
-            for (int x = 0; x < line.length; x++) {
-                if (grid[y][x] == 'x') {
-                    cnt++;
-                }
+        var grid = new char[lines.length][lines[0].length()];
+
+        for (int y = 0; y < lines.length; y++) {
+            var line = lines[y];
+            for (int x = 0; x < line.length(); x++) {
+                grid[y][x] = line.charAt(x);
             }
         }
-
-        return cnt;
+        return grid;
     }
 
 
@@ -77,7 +114,6 @@ public class Day04 {
             cnt++;
         }
 
-
         return cnt < 4;
     }
 
@@ -91,67 +127,4 @@ public class Day04 {
         }
         return grid[yPos][xPos];
     }
-
-    private static void print(char[][] grid) {
-        for (int y = 0; y < grid.length; y++) {
-            var line = grid[y];
-            for (int x = 0; x < line.length; x++) {
-                System.out.print(line[x]);
-            }
-            System.out.println();
-        }
-    }
-
-    public static long partTwo(String input) {
-        var lines = input.split("\n");
-
-        var grid = new char[lines.length][lines[0].length()];
-
-        for (int y = 0; y < lines.length; y++) {
-            var line = lines[y];
-            for (int x = 0; x < line.length(); x++) {
-                grid[y][x] = line.charAt(x);
-            }
-        }
-
-
-        var cnt = 0;
-        while(true) {
-            for (int y = 0; y < grid.length; y++) {
-                var line = grid[y];
-                for (int x = 0; x < line.length; x++) {
-                    if (grid[y][x] == '@') {
-                        var isok = check(grid, x, y);
-                        if (isok) {
-                            grid[y][x] = 'x';
-                        } else {
-                            grid[y][x] = grid[y][x];
-                        }
-                    } else {
-                        grid[y][x] = grid[y][x];
-                    }
-                }
-            }
-
-
-            int currentCnt = 0;
-            for (int y = 0; y < grid.length; y++) {
-                var line = grid[y];
-                for (int x = 0; x < line.length; x++) {
-                    if (grid[y][x] == 'x') {
-                        grid[y][x] = '.';
-                        currentCnt++;
-                    }
-                }
-            }
-
-            if (currentCnt == 0) {
-                break;
-            }
-
-            cnt += currentCnt;
-        }
-        return cnt;
-    }
-
 }
