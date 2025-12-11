2f0f105a1f68809272d38723bf8a414c9ffe57e8
Sebastian Meyer <git@ripa-alta.de>
2025-12-11 06:01:59 +0100

    WIP 10

--- src/test/java/de/aoc/y2025/Day10Test.java
+++ src/test/java/de/aoc/y2025/Day10Test.java
@@ -13,7 +13,7 @@ public class Day10Test extends AoCTest {
 
     @Test
     public void testPartOne() {
-//        assertEquals(2, Day10.partOne("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"));
+        assertEquals(2, Day10.partOne("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"));
 //        assertEquals(3, Day10.partOne("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}"));
 //        assertEquals(2, Day10.partOne("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"));
 //        assertEquals(7, Day10.partOne(input));
